9f75f806ca2c80920e672b860b13a40e1e498bdf
sonyak <kch9141@naver.com>
2022-02-02 18:00:49 +0900

    Update 2.py

--- Feb/2.py
+++ Feb/2.py
@@ -1,11 +1,11 @@
 import torch
 
-a = torch.randn(1, requires_grad = True)
+a = torch.randn((2,2), requires_grad = True)
 print(a)
-w1 = torch.randn(1, requires_grad = True)
-w2 = torch.randn(1, requires_grad = True)
-w3 = torch.randn(1, requires_grad = True)
-w4 = torch.randn(1, requires_grad = True)
+w1 = torch.randn((2,2), requires_grad = True)
+w2 = torch.randn((2,2), requires_grad = True)
+w3 = torch.randn((2,2), requires_grad = True)
+w4 = torch.randn((2,2), requires_grad = True)
 
 
 b = w1*a
@@ -13,6 +13,10 @@ c = w2*a
 
 d = w3*b + w4*c
 
-L = (10 - d)
+L = 10 - d
 
-L.backward()
+L.backward(torch.ones(L.size()))
+
+### 원래는 backward 는 스칼라형태의 input 에서만 가능하지만, 벡터형태의 input 에서도 이를 가능하게끔 하는 몇가지 조작이 있다.
+# 1.L.backward(torch.ones(L.size()))
+# 2.L = torch.sum(10-d)
